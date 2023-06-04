b0bb90225ee395aef101bf9ce60e39adcf0f8c9c
Evgenii Gerasimov <103824860+SawMassacre@users.noreply.github.com>
2023-06-05 03:48:05 +1000

    buttonback_done

---
+++ client/src/components/ButtonBack.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bckbtn">
+    <vs-button 
+        color="#76816f"
+        size="mini"
+        circle
+        icon
+        floating
+      >
+        <img src="../assets/images/left.svg" style="height: 3vh; width: 3vh;">
+      </vs-button>
+      </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    .bckbtn {
+        position: absolute;
+        top: 10vh;
+        right: 3vw;
+        z-index: 100;
+    }
+</style>
